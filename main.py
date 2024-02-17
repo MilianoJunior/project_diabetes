792efd88335bd367e95d2f36521486d5530ca005
MilianoJunior <jrmfilho37@gmail.com>
2024-02-17 10:17:38 -0300

    Init project

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Descrição: Modelo preditivo do dataset de diabetes
+Autor: Miliano F. de Oliveira Junior
+
+Requisitos:
+    Utilizar o dataset de Diabetes (visto em outra disciplina do curso)
+    Selecionar um modelo treinado e validado
+    Desenvolver uma aplicação utilizando Streamlit que permita o usuário entrar com os dados
+    Realizar a predição/aplicação do modelo treinado e retornar o resultado para o usuário
+    Capturar o feedback do usuário para poder avaliar a solução
+    Calcular métricas de avaliação e exibir os resultados na tela
+    Subir o código para um repositório Git
+    Realizar a implantação da solução na AWS – EC2
+
+'''
